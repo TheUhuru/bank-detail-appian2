fd14c5bcaa0fc440038ea7bc54ad37fc8e52dd32
TheUhuru <36730562+TheUhuru@users.noreply.github.com>
2020-10-15 11:34:29 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,15 +2,17 @@ const http = require('http');
 const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+const app = express();
+
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+
+  twiml.message('The Robots are coming! Head for the hills!');
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
 });
 
+http.createServer(app).listen(1337, () => {
+  console.log('Express server listening on port 1337');
+});
