e13409606e1234122474bf5396ca18aa60ccacfa
TheUhuru <36730562+TheUhuru@users.noreply.github.com>
2020-10-15 13:07:24 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ const app = express();
 app.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
 
+  // Access the message body and the number it was sent from.
+  console.log('Incoming message from ${req.body.From}: ${req.body.Body}');
+
   twiml.message('The Robots are coming! Head for the hills!');
 
   res.writeHead(200, {'Content-Type': 'text/xml'});
