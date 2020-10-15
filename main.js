3877cc05bc4033b97628bc9398d24e3bcf10694b
TheUhuru <36730562+TheUhuru@users.noreply.github.com>
2020-10-15 13:48:42 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -10,22 +10,6 @@ app.use(urlencoded({ extended: false }));
 var body;
 var from;
 
-app.post('/sms', (req, res) => {
-  const twiml = new MessagingResponse();
-
-  // Access the message body and the number it was sent from.
-  console.log(`Incoming message from ${req.body.From}: ${req.body.Body}`);
-  var body = req.body.Body;
-  var phone = req.body.Form;
-
-  twiml.message('The Robots are coming! Head for the hills!');
-
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
-});
-
-//	Lets send some stuff to Appian
-//
 var headers = {
     'Content-Type': 'application/json',
     'Appian-API-Key': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIwN2M1N2IzZi04NjlhLTQyNDEtOTFiZS0zMmNkOTk0MWYwZjQifQ.XyFoBX1PsAPjEIf-TZ_5oy7pV195b7RQMCvt4LOtp9k'
@@ -38,12 +22,29 @@ var options = {
     json: true,
     body: { body:body, from:from }
 }
-request(options, function (error, response, body) {
-    if (error) {
-        //do something
-    }
-    console.log(body)//do something with response
-})
+
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+
+  // Access the message body and the number it was sent from.
+  console.log(`Incoming message from ${req.body.From}: ${req.body.Body}`);
+  var body = req.body.Body;
+  var phone = req.body.Form;
+
+  twiml.message('The Robots are coming! Head for the hills!');
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+
+//	Lets send some stuff to Appian
+//
+
+});
+
+
+
+
+
 
 //http.createServer(app).listen(1337, () => {
 //  console.log('Express server listening on port 1337');
