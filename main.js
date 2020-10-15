cd23ec926057bc537adb31363250f20a9c0d2f61
TheUhuru <36730562+TheUhuru@users.noreply.github.com>
2020-10-15 13:34:11 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,16 +2,22 @@ const http = require('http');
 const express = require('express');
 const { urlencoded } = require('body-parser');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const axios = require('axios');
 
 const app = express();
 app.use(urlencoded({ extended: false }));
+
+var body;
+var from;
 
 app.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
 
   // Access the message body and the number it was sent from.
   console.log(`Incoming message from ${req.body.From}: ${req.body.Body}`);
-
+  var body = req.body.Body;
+  var phone = req.body.Form;
+  
   twiml.message('The Robots are coming! Head for the hills!');
 
   res.writeHead(200, {'Content-Type': 'text/xml'});
